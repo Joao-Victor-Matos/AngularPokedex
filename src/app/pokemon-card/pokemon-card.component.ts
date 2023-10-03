99ae24f654fdab3d181de057932ec1973eedb6d8
JoaoMatos <queissotropa127@gmail.com>
2023-10-03 16:00:43 -0300

    feat: all data is now reactive

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.css'],
 })
-export class PokemonCardComponent {}
+export class PokemonCardComponent {
+  @Input()
+  public pokemon!: Pokemon;
+
+  public leadingZero(str: string | number, size: number): string {
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+    return s;
+  }
+}
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Pokemon } from 'src/_model/Pokemon';
+import { Type } from 'src/_model/Type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css'],
 })
-export class PokemonListComponent {}
+export class PokemonListComponent {
+  public pokemons: Pokemon[] = [
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbassaur',
+      types: [Type.Grass, Type.Poison],
+    },
+  ];
+}
